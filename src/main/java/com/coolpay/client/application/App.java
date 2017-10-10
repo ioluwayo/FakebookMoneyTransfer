53264073bb44f88b4773dac06e1084321f0f1f93
ioluwayo <ioluwayo@ryerson.ca>
2017-10-10 23:03:44 +0100

    Implemented CLI parser for handling user specified credentials

--- src/main/java/com/coolpay/client/application/App.java
+++ src/main/java/com/coolpay/client/application/App.java
@@ -9,6 +9,13 @@ import com.coolpay.client.CoolPayClient;
 import com.coolpay.client.exception.CoolPayClientException;
 import com.coolpay.client.model.Login;
 import com.sun.jersey.api.client.ClientHandlerException;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 /**
  *
@@ -16,17 +23,41 @@ import com.sun.jersey.api.client.ClientHandlerException;
  */
 public class App {
     public static void main(String[]args){
-        String username, apikey;
-        // username and apikey can be passed as arguments
-         if(args.length>0){
-             username = args[0];
-             apikey = args[1];
-         }else{
-             //defualt credentials will be used
-             username = "IbukunO";
-             apikey = "BF3B477DAE97B128";
-         }
-         Login login = new Login(username,apikey);
+        String username = "IbukunO" , apikey = "BF3B477DAE97B128"; //defuault credentials
+        
+        /**
+         * command line parser options set up..
+         * Optional..will only be used when provided by user
+         */
+        Options options = new Options();
+        Option usernameOption = new Option("u", "username", true, "username for coolpay client");
+        usernameOption.setRequired(false);
+        usernameOption.setArgName("COOLPAY USERNAME");
+        options.addOption(usernameOption);
+        Option apikeyOption = new Option("a", "apikey", true, "apikey for coolpay client");
+        apikeyOption.setRequired(false);
+        apikeyOption.setArgName("COOLPAY APIKEY");
+        options.addOption(apikeyOption);
+        CommandLineParser commandLineParser = new DefaultParser();
+        HelpFormatter helpFormatter = new HelpFormatter();
+        CommandLine commandLine;
+        try {
+            commandLine = commandLineParser.parse(options, args); // will throw parse exception
+            if (commandLine.hasOption("apikey") && commandLine.hasOption("username")){
+                //overide default values
+                username = commandLine.getOptionValue("username");
+                apikey = commandLine.getOptionValue("apikey");
+                System.out.println(username+" "+apikey);
+            }
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            helpFormatter.printHelp("Fakebook Moneytransfer", options);
+            System.exit(1);
+            return;
+        }
+        
+        // everything is supossedly good at this point...so proceed
+        Login login = new Login(username,apikey);
          
          try {
                 // insantiate just one CoolPayClient instance for this application
